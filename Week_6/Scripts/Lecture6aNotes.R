adde34500408390b18e12ffe984edd469670f188
Amanda Chiach <amandachiachi@gmail.com>
2021-03-03 08:58:43 -0800

    Week 6a lecture notes

---
+++ Week_6/Scripts/Lecture6aNotes.R
@@ -0,0 +1,13 @@
+####Week 6a Lecture Notes: RMarkdown1 ####
+####Created by Amanda Chiachi####
+####Last edited 03-1-2021####
+
+###Load Libraries####
+library(tidyverse)
+library(here)
+library(PNWColors)
+
+#Metadata: YAML 
+#text 
+#code
+#output
